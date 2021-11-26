7238a63434989c2f47e65bfde10189a80ed0bf91
DarthPjotr <DarthPjotr@users.noreply.github.com>
2021-11-26 13:48:03 +0100

    .

--- startminikube.ps1
+++ startminikube.ps1
@@ -1,4 +1,4 @@
-minikube start --cni=calico --insecure-registry=10.0.0.0/24 --wait-timeout 30m0s
+minikube start --cni=calico --insecure-registry=10.0.0.0/24 --wait-timeout 30m0s --driver=hyperv --container-runtime=containerd
 # -p <name>
 # --kubernetes-version=v1.20.2 
 # --kubernetes-version=v1.20.0

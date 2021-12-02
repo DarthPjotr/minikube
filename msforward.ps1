8ce5e1ba05917d8586abb1cc362bb3a798d4caae
DarthPjotr <DarthPjotr@users.noreply.github.com>
2021-12-02 16:25:27 +0100

    myswitch

---
+++ msforward.ps1
@@ -0,0 +1,7 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+
+Get-NetIPInterface | where { $_.InterfaceAlias -eq 'vEthernet (myswitch)' -or $_.InterfaceAlias -eq 'vEthernet (Default Switch)' } | Set-NetIPInterface -Forwarding Enabled
+# Disable-NetAdapterLso -Name 'vEthernet (WSL)'
